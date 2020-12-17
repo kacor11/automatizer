204fbc7c55e0959d2d9a9c713f8cbf51983432d6
kacor11 <kubakacor@gmail.com>
2020-12-17 21:46:15 +0100

    Rewrie PlaybookExecutor

--- src/main/java/pl/kocjan/automatizer/domain/playbook/PlaybookExecutor.java
+++ src/main/java/pl/kocjan/automatizer/domain/playbook/PlaybookExecutor.java
@@ -3,6 +3,8 @@ package pl.kocjan.automatizer.domain.playbook;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import com.jcraft.jsch.Session;
 
@@ -11,22 +13,40 @@ import lombok.RequiredArgsConstructor;
 import pl.kocjan.automatizer.domain.common.vavr.Error;
 import pl.kocjan.automatizer.domain.common.vavr.Success;
 import pl.kocjan.automatizer.domain.host.Host;
-import pl.kocjan.automatizer.domain.host.port.HostRepository;
+import pl.kocjan.automatizer.domain.host.HostFacade;
 import pl.kocjan.automatizer.domain.playbook.dto.PlaybookDto;
-import pl.kocjan.automatizer.domain.playbook.dto.TaskResultDto;
 import pl.kocjan.automatizer.domain.playbook.port.HostConnection;
 
 @RequiredArgsConstructor
-public class PlaybookExecutor {
+class PlaybookExecutor {
 	private final HostConnection hostConnection;
-	private final HostRepository hostRepository;
+	private final PlaybookCreator playbookCreator;
+	private final HostFacade hostFacade;
 	
-	Either<Error, Success> execute(PlaybookDto playbookDto) {
-		return 
-
-	}		
+	Either<Error, List<Either<Error, Host>>> execute(PlaybookDto playbookDto) {
+		return playbookCreator.createForGroup(playbookDto)
+			.map(playbooks -> executePlaybookOnMultipleHosts(playbooks));
+	}
 	
-	private Either<Error, Success> executePlaybookOnSingleHost(Playbook playbook) {
+	List<Either<Error, Host>> executePlaybookOnMultipleHosts(List<Playbook> playbooks) {
+		
+		List<CompletableFuture<Either<Error, Host>>> futureList = 
+				playbooks.stream()
+				.map(playbook -> {
+					return CompletableFuture.supplyAsync(() -> executePlaybookOnSingleHost(playbook));
+				})
+				.collect(Collectors.toList());
+		
+		CompletableFuture<Void> allFuturesResult =
+				CompletableFuture.allOf(futureList.toArray(new CompletableFuture[0]));
+		
+		return allFuturesResult.thenApply(v -> 
+				futureList.stream()
+				.map(future -> future.join())
+				.collect(Collectors.<Either<Error, Host>>toList())).join();				
+	}
+	
+	 Either<Error, Host> executePlaybookOnSingleHost(Playbook playbook) {
 		Host host = playbook.getHost();
 		Either<Error, Session> session = hostConnection.establishConnection(host);
 		if(session.isLeft()) {
@@ -40,12 +60,12 @@ public class PlaybookExecutor {
 				task.executionResult(Task.Result.FAILED);
 			} else {
 				task.executionResult(Task.Result.SUCCEED);
-		}
+			}
 			host.submitTask(task);
-			return Either.right(new Success());
+		}
+		Either<Error, Success> updateHostData = hostFacade.updateHost(host);
+		return updateHostData.isLeft() ? Either.left(updateHostData.getLeft()) : Either.right(host);
 	}
-
-	
 
 	
 }
