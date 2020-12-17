88f377a559408cdbdaa68224a16338996e191bf2
kacor11 <kubakacor@gmail.com>
2020-12-17 21:49:29 +0100

    Add error msg

--- src/main/java/pl/kocjan/automatizer/adapter/connector/ConnectionError.java
+++ src/main/java/pl/kocjan/automatizer/adapter/connector/ConnectionError.java
@@ -7,7 +7,8 @@ import pl.kocjan.automatizer.domain.common.vavr.Error;
 public enum ConnectionError implements Error {
 	KNOWN_HOSTS_ERROR("Can't locate known hosts file"),
 	HOST_CONNECTION_ERROR("Can't connect to the host"),
-	HOST_EXECUTION_ERROR("Running command on remote host failed");
+	HOST_EXECUTION_ERROR("Running command on remote host failed"),
+	COMMAND_EXECUTION_ERROR("Execution on remote host failed");
 	
 	private final String error;
 	
